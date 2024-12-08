b31f632d04fded760274e397a04061a3920f810f
Sema Butsyk <semakontakt@mail.ru>
2024-12-08 17:11:20 +0500

    Ver 2

--- test_1.py
+++ test_1.py
@@ -17,13 +17,6 @@ def test_pdf_file():
         assert "Тестовый PDF-документ" in page.extract_text(0)
 
 
-def test_xlsx_file():
-    with ZipFile("new.zip", 'r') as zip_archive:
-        xslx_file = load_workbook(zip_archive.open("file_2.xlsx"))
-        sheet = xslx_file['TemplateImportEmpl']
-        assert sheet.cell(row=2, column=1).value == "CN001"
-
-
 def test_csv_file():
     with ZipFile("new.zip", 'r') as zip_archive:
         data = StringIO(zip_archive.read("file_3.csv").decode('utf-8'))
@@ -32,3 +25,10 @@ def test_csv_file():
         for _ in data_converted:
             for row in data_converted:
                 assert "Дорожка на стол Веселые снеговики (50х150см)" in row
+
+
+def test_xlsx_file():
+    with ZipFile("new.zip", 'r') as zip_archive:
+        xslx_file = load_workbook(zip_archive.open("file_2.xlsx"))
+        sheet = xslx_file['TemplateImportEmpl']
+        assert sheet.cell(row=2, column=1).value == "CN001"
